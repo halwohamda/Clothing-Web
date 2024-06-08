2592fbc0b2781cae9f799bb698ff1765aaa5759f
halwohamda <halwohamda@gmail.com>
2024-06-08 15:22:04 -0800

    done

--- Works/src/Components/Works.jsx
+++ Works/src/Components/Works.jsx
@@ -10,7 +10,7 @@ const Works = () => {
          <p className='mt-3'>Lorem ipsum dolor sit amet consectetur adip <br /> a nemo iste placeat.</p>
  
     <div className="flex w-full items-center md:flex-row justify-center flex-col gap-8 mt-8  ">
-        <div className=" bg-slate-200 p-[15px] rounded-md " >
+        <div className=" p-[15px] rounded-md  " >
         <img src={masxaf} alt="" className='w-[250px] h-[150px]' />
         </div>
 
@@ -22,15 +22,16 @@ const Works = () => {
     </div>
     
 <div className="flex w-full items-center md:flex-row justify-center mt-8 gap-8  flex-col ">
-    <div className=" bg-slate-200 p-[15px] rounded-md" >
-        <img src={Doctor} alt="" className='w-[250px] h-[150px]' />
-        </div>
+
 
         <div className="gap-5">
      <h3 className='font-bold  text-teal-700'>Doctor Appointmenr</h3>
      <p>Lorem ipsum dolor sit, amet consectetur <br /> adipisicing elit. Repell</p>
      <button className='mt-10 px-6 py-1 border-teal-700 bg-white hover:text-white transition-all  rounded-lg border-2 text-teal-700   hover:bg-teal-600 '>View details</button>
      </div>
+     <div className="  p-[15px] rounded-md" >
+        <img src={Doctor} alt="" className='w-[250px] h-[150px]' />
+        </div>
      </div>
  </div>
 </div>
